8086180bff495f47a0599f80b81d6aa8da0b183e
Alex170392 <alex.maliushytski@gmail.com>
2024-05-18 21:25:04 +0300

    adding new lines

--- file-converter/src/main/java/dev/alex/maliushytski/fileconverter/service/csv/CsvGenerator.java
+++ file-converter/src/main/java/dev/alex/maliushytski/fileconverter/service/csv/CsvGenerator.java
@@ -3,6 +3,9 @@ package dev.alex.maliushytski.fileconverter.service.csv;
 public class CsvGenerator {
     public static void main(String[] args) {
 
+        System.out.println("new line1!!!");
+        System.out.println("new line2!!!");
+
     }
 
     //todo создавать пустые csv файлы
@@ -14,5 +17,7 @@ public class CsvGenerator {
     //moloko1|banan1|kolbasa1
     //moloko2|banan2|kolbasa2
     //moloko3|banan3|kolbasa3
+
+
 
 }
